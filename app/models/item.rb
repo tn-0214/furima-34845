a989b710315b51dc2cc2ad0326a37179a31c6cab
tn-0214 <wataru.nishikiori1209@gmail.com>
2021-03-09 17:01:08 +0900

    ActiveHashの導入〜Validation設定

---
+++ app/models/item.rb
@@ -0,0 +1,23 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_one_attached :image
+  belongs_to :user
+  belongs_to :ship_form
+  belongs_to :condition
+  belongs_to :category
+  belongs_to :etd
+  belongs_to :postage
+
+  with_options presence: true, numericality: { other_than: 1 }  do
+    validates :category_id
+    validates :condition_id
+    validates :postage_id
+    validates :ship_form_id
+    validates :etd_id
+  end
+
+  validates :price, presence: true, numericality: {:greater_than_or_equql_to => 300, :less_than_or_equal_to => 9999999}, format: { with: /^[0-9]+$/, message: '半角数字を使用してください' } 
+
+  validates :image, presence: true
+
+end

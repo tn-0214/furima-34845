8fb90328cdc6ea2ce18516c973f99a468f0929f5
tn-0214 <wataru.nishikiori1209@gmail.com>
2021-03-09 21:39:04 +0900

    商品出品フォームの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,7 +16,7 @@ class Item < ApplicationRecord
     validates :etd_id
   end
 
-  validates :price, presence: true, numericality: {:greater_than_or_equql_to => 300, :less_than_or_equal_to => 9999999}, format: { with: /^[0-9]+$/, message: '半角数字を使用してください' } 
+  validates :price, presence: true, numericality: {:greater_than_or_equql_to => 300, :less_than_or_equal_to => 9999999}, format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' } 
 
   validates :image, presence: true
 
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Name.initials(number: 2)}
+    last_name {"山田"}
+    first_name {"太郎"}
+    last_name_kana {"ヤマダ"}
+    first_name_kana {"タロウ"}
+    email {Faker::Internet.free_email}
+    password = "abc123"
+    password {password}
+    password_confirmation {password}
+    birthday {Faker::Date.backward}
   end
 end
